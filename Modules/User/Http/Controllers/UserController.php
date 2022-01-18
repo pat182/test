2c49b4fbc0a4065c46db27e205f9b95162c52f31
pat182 <patathome182@gmail.com>
2022-01-18 19:03:48 +0800

    remove unnecessary files

--- Modules/User/Http/Controllers/UserController.php
+++ Modules/User/Http/Controllers/UserController.php
@@ -39,6 +39,7 @@ class UserController extends Controller
     public function updateProfile(UserProfileRequest $request)
     {
         $payload = $request->payload();
+        dd($payload);
         $user = request()->user();
         $this->status = $this->userService->updateProfile($payload,$user->user_id);
         return response()->json([
